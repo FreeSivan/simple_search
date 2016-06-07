38b8aedc0b80c2e439f4b220eeef9e5c03f093a1
xiwen.yxw <xiwen.yxw@alibaba-inc.com>
2016-06-07 10:01:01 +0800

    simp_search的后续思考

---
+++ src/main/java/com/xiwen/yxw/domain/requestBean.java
@@ -0,0 +1,27 @@
+package com.xiwen.yxw.domain;
+
+/**
+ * Created by xiwen.yxw on 2016/6/6.
+ */
+public class RequestBean {
+
+    private String searchField;
+
+    private String searchKey;
+
+    public String getSearchField() {
+        return searchField;
+    }
+
+    public void setSearchField(String searchField) {
+        this.searchField = searchField;
+    }
+
+    public String getSearchKey() {
+        return searchKey;
+    }
+
+    public void setSearchKey(String searchKey) {
+        this.searchKey = searchKey;
+    }
+}
